b5391548207e2c90981913c7d47543c5ead6aa81
hydukovich <148716031+hydukovich@users.noreply.github.com>
2025-10-14 17:32:10 -0400

    Update element.h

---
+++ element.h
@@ -0,0 +1,24 @@
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+#include <iostream>
+
+template<typename T>
+class Element {
+private:
+    T data;
+    long key;
+
+public:
+    Element<T>* prev;
+    Element<T>* next;
+
+    // Constructors
+    Element(const T& data_, long key_)
+        : data(data_), key(key_), prev(nullptr), next(nullptr) {}
+
+    long get_key() const { return key; }
+    T get_data() const { return data; }
+};
+
+#endif
